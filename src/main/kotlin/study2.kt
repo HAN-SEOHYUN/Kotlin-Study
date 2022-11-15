b7fd435536b18832a153e28d7106f23049258baf
HAN-SEOHYUN <000312@daum.net>
2022-11-15 11:02:23 +0900

    컬렉션

--- src/main/kotlin/study2.kt
+++ src/main/kotlin/study2.kt
@@ -19,4 +19,40 @@ fun main(args:Array<String>){
     println(map["B"])
     println(map["C"])
     println(map["D"])
+
+    var map2 = HashMap<String, String>()
+    map2["A"] = "Joongsoo"
+    map2["B"] = "Younghwan"
+    map2["C"] = "Minji"
+
+    println(map2["A"])
+    println(map2["B"])
+    println(map2["C"])
+
+    var map3 = hashMapOf<String, String>()
+    map3["A"] = "Joongsoo"
+    map3["B"] = "Joongsoo"
+    map3["C"] = "Joongsoo"
+
+    println(map3["A"])
+    println(map3["B"])
+    println(map3["C"])
+
+    var intRange = IntRange(0,10)
+    var charRange = CharRange('a','e')
+    var longRange = LongRange(11,20)
+
+    var intRange1 = 0..10
+    var charRange1 = 'a' .. 'e'
+
+
+    for(i in intRange)
+        println(i)
+
+    for(i in charRange)
+        println(i)
+
+    for(i in longRange)
+        println(i)
+
 }
